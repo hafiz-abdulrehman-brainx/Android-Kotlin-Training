33aa08f27c3ae44068193c12e047768f69bb78a3
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-19 15:21:59 +0500

    view model simple example

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -4,11 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),LifecycleOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var btn = findViewById<Button>(R.id.btnApply)
+        var txt = findViewById<TextView>(R.id.txt)
+        var viewmodel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        txt.text=viewmodel.number.toString()
+        btn.setOnClickListener{
+            viewmodel.addNumber()
+            txt.text=viewmodel.number.toString()
+        }
+
     }
+
 
 }
