ce82e5488edfdce36476ef12419bd74fc73eceb9
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-21 20:38:56 +0500

    todo list with simple recycler view

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.login
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val todolist = mutableListOf(
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false),
+            Todo("Do this",false)
+         )
+        val adapter = TodoAdapter(todolist)
+        recycle.adapter = adapter
+        recycle.layoutManager = LinearLayoutManager(this)
+
+
+        btnAdd.setOnClickListener {
+            val title = txt.text.toString()
+            val todo = Todo(title, false)
+            todolist.add(todo)
+            adapter.notifyItemChanged(todolist.size-1)
+        }
     }
 
 }
