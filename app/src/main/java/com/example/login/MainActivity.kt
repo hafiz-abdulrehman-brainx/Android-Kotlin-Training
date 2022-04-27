955f5506dfc2a2e7e473387f334f7c46a561222d
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-27 15:30:09 +0500

    view binding with acticity and fragment

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import com.example.login.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+        // type safe and null safe view objects associated with this activity
+        txtOutput?.text = "new text with view binding" // make it nullable because it does not exist in landscape mode
+        btnClick.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button clicked",Toast.LENGTH_SHORT).show()
+            val fragment = mainFragment()
+             supportFragmentManager
+                 .beginTransaction()
+                 .add(R.id.frame_layout,fragment)
+                 .commit()
+        }
+        }
     }
 
 }
